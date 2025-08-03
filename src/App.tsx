e59b9590c939a6ec4b05dfb36801a726cc4a3e3d
jaume vidal <jaumevibu@gmail.com>
2025-08-03 17:41:59 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section class="w-full grid grid-rows-[auto_1fr_auto] min-h-screen">
+      <header class="bg-red-600 text-center">header</header>
+      <main class="bg-green-600 text-center">main</main>
+      <footer class="bg-blue-600 text-center">footer</footer>
+    </section>
+  );
+}
+
+export default App;
